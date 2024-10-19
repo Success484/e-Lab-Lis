aed1067b763264ccd31eb38cf11d1d0c0e3c6158
Success <successsimeon484@gmail.com>
2024-10-19 22:49:05 +0100

    feat: updating api

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 function toggle_sidebar() {
-    const sidebar = document.querySelector(".side-nav");
-    sidebar.classList.toggle("side");
+  const sidebar = document.querySelector(".side-nav");
+  sidebar.classList.toggle("side");
+}
+function handleSidebar() {
+  const sidebar = document.querySelector(".side-nav");
+
+  if (window.innerWidth >= 1221) {
+    sidebar.classList.add("side");
+  } else {
+    sidebar.classList.remove("side");
   }
-  function handleSidebar() {
-    const sidebar = document.querySelector(".side-nav");
-  
-<<<<<<< HEAD
-    if (window.innerWidth >= 1221) {
-      sidebar.classList.add("side");
-    } else {
-      sidebar.classList.remove("side");
+}
+window.addEventListener("resize", handleSidebar);
+window.addEventListener("load", handleSidebar);
+
+// fetching api
+
+const close_medicine = document.querySelector(".close-medicine");
+close_medicine.addEventListener("click", () => {
+  const display = document.querySelector(".medicine-section");
+  display.style.display = "none";
+});
+
+const close_medicine_list = document.querySelector(".close-medicine-list");
+close_medicine_list.addEventListener("click", () => {
+  const display = document.querySelector(".medicine-section-list");
+  display.style.display = "none";
+});
+
+let medicines = [];
+const inputField1 = document.getElementById("input1");
+const inputField3 = document.getElementById("input3");
+
+document.getElementById("input1").addEventListener("click", async () => {
+  document.querySelector(".loader").style.display = "block";
+  try {
+    const display = document.querySelector(".medicine-section");
+    if (medicines.length === 0) {
+      const response = await fetch(
+        "https://cliniqueplushealthcare.com.ng/prescriptions/drug_class"
+      );
+      medicines = await response.json();
     }
+    displayMedicines(medicines);
+    display.style.display = "block";
+    document.querySelector(".loader").style.display = "none";
+  } catch (err) {
+    console.error("Error fetching data:", err);
   }
-  window.addEventListener("resize", handleSidebar);
-  window.addEventListener("load", handleSidebar);
-  
-  // fetching api
-  
-  const close_medicine = document.querySelector('.close-medicine')
-      close_medicine.addEventListener('click', () => {
-          const display = document.querySelector('.medicine-section')
-          display.style.display = 'none'
-=======
+});
+
+document.getElementById("input1").addEventListener("input", () => {
+  const searchQuery = document.getElementById("input1").value.toLowerCase();
+  const filteredMedicines = medicines.filter((medicine) =>
+    medicine.name.toLowerCase().includes(searchQuery)
+  );
+  displayMedicines(filteredMedicines);
+});
 
 function displayMedicines(data) {
   const medicineContainer = document.querySelector(".medicine-brand");
@@ -31,139 +66,84 @@ function displayMedicines(data) {
       const section = document.createElement("section");
       const p = document.createElement("p");
       p.textContent = medicine.name;
-      p.addEventListener('click', async () => {
-        document.getElementById("input2").focus()
-        document.querySelector('.medicine-section').style.display = 'none'
-        inputField1.value = p.textContent
+      p.addEventListener("click", async () => {
+        document.getElementById("input2").focus();
+        document.querySelector(".medicine-section").style.display = "none";
+        inputField1.value = p.textContent;
         await fetchMedicinesByCategory(medicine.id);
-        document.querySelector('.medicine-section-list').style.display = 'block'
->>>>>>> df14d79f75e702d045850cc60c6c16be4c5c560f
-      })
-  
-  const close_medicine_list = document.querySelector('.close-medicine-list')
-      close_medicine_list.addEventListener('click', () => {
-          const display = document.querySelector('.medicine-section-list')
-          display.style.display = 'none'
-      })
-  
-  let medicines = [];
-  const inputField1 = document.getElementById('input1')
-  const inputField3 = document.getElementById('input3')
-  
-  document.getElementById("input1").addEventListener("click", async () => {
-      document.querySelector('.loader').style.display = 'block';
-      try {
-      const display = document.querySelector('.medicine-section')
-      if (medicines.length === 0) {
-          const response = await fetch(
-              "https://cliniqueplushealthcare.com.ng/prescriptions/drug_class"
-          );
-          medicines = await response.json();
-      }
-      displayMedicines(medicines);
-      display.style.display = 'block'
-      document.querySelector('.loader').style.display = 'none';
-    } catch (err) {
-      console.error("Error fetching data:", err);
-    }
-  });
-  
-  document.getElementById("input1").addEventListener("input", () => {
-    const searchQuery = document.getElementById("input1").value.toLowerCase();
-    const filteredMedicines = medicines.filter((medicine) =>
-      medicine.name.toLowerCase().includes(searchQuery)
-    );
-    displayMedicines(filteredMedicines);
-  });
-    
-  
-  function displayMedicines(data) {
-    const medicineContainer = document.querySelector(".medicine-brand");
-    medicineContainer.innerHTML = "";
-    if (data.length > 0) {
-      data.forEach((medicine) => {
-        const section = document.createElement("section");
-        const p = document.createElement("p");
-        p.textContent = medicine.name;
-        p.addEventListener('click', async () => {
-          document.getElementById("input2").focus()
-          document.querySelector('.medicine-section').style.display = 'none'
-          inputField1.value = p.textContent
-          await fetchMedicinesByCategory(medicine.id);
-          document.querySelector('.medicine-section-list').style.display = 'block'
-        })
-        section.appendChild(p);
-        medicineContainer.appendChild(section);
+        document.querySelector(".medicine-section-list").style.display =
+          "block";
       });
-    } else {
-      const noResult = document.createElement("p");
-      noResult.textContent = "No results found";
-      medicineContainer.appendChild(noResult);
-    }
-<<<<<<< HEAD
+      section.appendChild(p);
+      medicineContainer.appendChild(section);
+    });
+  } else {
+    const noResult = document.createElement("p");
+    noResult.textContent = "No results found";
+    medicineContainer.appendChild(noResult);
   }
-  
-  
-  const fetchMedicinesByCategory = async (categoryId) => {
-      try {
-        const response = await fetch(`https://cliniqueplushealthcare.com.ng/prescriptions/get_drug_class_by_id/${categoryId}`);
-        const medicines = await response.json();
-        const reversedMedicines = medicines.reverse(); 
-        if (reversedMedicines && Array.isArray(reversedMedicines)) {
-          displaySecondMedicines(reversedMedicines); 
-        } else {
-          console.error('No valid medicines data returned');
-        }
-      } catch (err) {
-        console.error("Error fetching medicines:", err);
-      }
-    };
-  
-  
-    function displaySecondMedicines(medicines) {
-      const medicineListContainer = document.querySelector(".medicine-brand-list");
-      medicineListContainer.innerHTML = ""; 
-      medicines.forEach((medicine) => {
-        const section = document.createElement("section");
-        const p = document.createElement("p");
-        p.textContent = medicine.medicine_name;
-    
-        p.addEventListener('click', () => {
-          document.getElementById('input2').value = p.textContent;
-          document.querySelector('.medicine-section-list').style.display='none'
-          inputField3.focus()
-        });
-    
-        section.appendChild(p);
-        medicineListContainer.appendChild(section);
-      });
+}
+
+const fetchMedicinesByCategory = async (categoryId) => {
+  try {
+    const response = await fetch(
+      `https://cliniqueplushealthcare.com.ng/prescriptions/get_drug_class_by_id/${categoryId}`
+    );
+    const medicines = await response.json();
+    const reversedMedicines = medicines.reverse();
+    if (reversedMedicines && Array.isArray(reversedMedicines)) {
+      displaySecondMedicines(reversedMedicines);
+    } else {
+      console.error("No valid medicines data returned");
     }
-    
-  
-    document.querySelector('.table-form').addEventListener('submit', function(e) {
-      e.preventDefault();
-  
-      const medicineBrand = document.getElementById('input1').value;
-      const medicineName = document.getElementById('input2').value;
-      const dose = document.getElementById('input3').value;
-      const frequency = document.getElementById('frequency').value;
-      const duration = document.getElementById("input4").value
-      const second_duration = document.querySelector('.second_duration').value;
-      const instructions = document.getElementById('input5').value;
-  
-    const tbody = document.querySelector('.table-1-tbody');
-  
-    const noDataRow = document.getElementById('no-data-row');
-  
-    if (noDataRow) {
-        noDataRow.remove();
-    }
-  
-    const rowCount = tbody.rows.length;
-  
-    const newRow = document.createElement('tr');
-  
-     newRow.innerHTML = `
+  } catch (err) {
+    console.error("Error fetching medicines:", err);
+  }
+};
+
+function displaySecondMedicines(medicines) {
+  const medicineListContainer = document.querySelector(".medicine-brand-list");
+  medicineListContainer.innerHTML = "";
+  medicines.forEach((medicine) => {
+    const section = document.createElement("section");
+    const p = document.createElement("p");
+    p.textContent = medicine.medicine_name;
+
+    p.addEventListener("click", () => {
+      document.getElementById("input2").value = p.textContent;
+      document.querySelector(".medicine-section-list").style.display = "none";
+      inputField3.focus();
+    });
+
+    section.appendChild(p);
+    medicineListContainer.appendChild(section);
+  });
+}
+
+document.querySelector(".table-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const medicineBrand = document.getElementById("input1").value;
+  const medicineName = document.getElementById("input2").value;
+  const dose = document.getElementById("input3").value;
+  const frequency = document.getElementById("frequency").value;
+  const duration = document.getElementById("input4").value;
+  const second_duration = document.querySelector(".second_duration").value;
+  const instructions = document.getElementById("input5").value;
+
+  const tbody = document.querySelector(".table-1-tbody");
+
+  const noDataRow = document.getElementById("no-data-row");
+
+  if (noDataRow) {
+    noDataRow.remove();
+  }
+
+  const rowCount = tbody.rows.length;
+
+  const newRow = document.createElement("tr");
+
+  newRow.innerHTML = `
      <td class="first-td">${rowCount + 1}</td>
      <td>
        <section>
@@ -176,46 +156,38 @@ function displayMedicines(data) {
      <td>${instructions}</td>
      <td class="remove-cell"><p class="remove">Remove</p></td>
    `;
-  
-   tbody.appendChild(newRow);
-  
-   document.querySelector('.table-form').reset();
-    })
-  
-    document.querySelector('.table-1-tbody').addEventListener('click', function(e) {
-      if (e.target.classList.contains('remove')) {
-        e.target.closest('tr').remove();
-      }
-    });
-  
-    function checkForEmptyTable() {
-      const tbody = document.querySelector('.table-1-tbody');
-      if (tbody.rows.length === 0) {
-          const noDataRow = document.createElement('tr');
-          noDataRow.id = 'no-data-row';
-          noDataRow.innerHTML = `
+
+  tbody.appendChild(newRow);
+
+  document.querySelector(".table-form").reset();
+});
+
+document
+  .querySelector(".table-1-tbody")
+  .addEventListener("click", function (e) {
+    if (e.target.classList.contains("remove")) {
+      e.target.closest("tr").remove();
+    }
+  });
+
+function checkForEmptyTable() {
+  const tbody = document.querySelector(".table-1-tbody");
+  if (tbody.rows.length === 0) {
+    const noDataRow = document.createElement("tr");
+    noDataRow.id = "no-data-row";
+    noDataRow.innerHTML = `
           <td colspan="7" style="text-align: center; font-weight: bold; padding-bottom: 5px;">No Prescribed drug yet</td>        `;
-          tbody.appendChild(noDataRow);
-      }
+    tbody.appendChild(noDataRow);
   }
-  document.addEventListener('DOMContentLoaded', checkForEmptyTable);
-  
-  document.querySelector('.done-pricribing-div').addEventListener('click', ()=>{
-      document.querySelector('.save-pricription-form').style.display='block'
-  })
-  
-  const remarkInput = document.querySelector('.save-pricription-form').addEventListener('submit', (e)=>{
-      e.preventDefault()
-  })
-=======
 }
-document.addEventListener('DOMContentLoaded', checkForEmptyTable);
+document.addEventListener("DOMContentLoaded", checkForEmptyTable);
 
-document.querySelector('.done-pricribing-div').addEventListener('click', ()=>{
-    document.querySelector('.save-pricription-form').style.display='block'
-})
+document.querySelector(".done-pricribing-div").addEventListener("click", () => {
+  document.querySelector(".save-pricription-form").style.display = "block";
+});
 
-const remarkInput = document.querySelector('.save-pricription-form').addEventListener('submit', (e)=>{
-    e.preventDefault()
-})
->>>>>>> df14d79f75e702d045850cc60c6c16be4c5c560f
+const remarkInput = document
+  .querySelector(".save-pricription-form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
